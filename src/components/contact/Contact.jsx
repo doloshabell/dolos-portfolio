1fad686283532d924a0199439d358404d8ccc3f7
doloshabell <habelldol@gmail.com>
2022-03-29 23:36:05 +0700

    done all component and function portfolio web

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-import './contact.css'
+import React, { useRef } from 'react';
+import './contact.css';
+import { SiGmail } from 'react-icons/si';
+import { SiWhatsapp } from 'react-icons/si';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_rftg4z3', 'template_1ay0vah', form.current, 'd6YqxWRwTyJmfdNGN')
+
+    e.target.reset();    
+  }
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <SiGmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>habelldol@gmail.com</h5>
+            <a href="mailto:habelldol@gmail.com" target="_blank">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <SiWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>0821-1489-8489</h5>
+            <a href="https://api.whatsapp.com/send?phone=6282114898489" target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import './footer.css';
+import { SiFacebook } from 'react-icons/si';
+import { SiInstagram } from 'react-icons/si';
+import { RiHeartFill } from 'react-icons/ri'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">Habell Dolosha Purba</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Service</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><SiFacebook /></a>
+        <a href="https://instagram.com"><SiInstagram /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>
+          &copy; Habell Dolosha Purba Portfolio - 2022 | Hope U Like My Portfolio Website <RiHeartFill />
+        </small>
+      </div>
+    </footer>
   )
 }
 
